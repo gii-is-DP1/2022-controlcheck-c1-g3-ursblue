4e7497d654ba56f786dc84eed5c58047cb6aebca
ursblue <ugsugs333@gmail.com>
2022-11-03 17:07:53 +0100

    .

--- src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomService.java
+++ src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomService.java
@@ -2,17 +2,29 @@ package org.springframework.samples.petclinic.recoveryroom;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class RecoveryRoomService {
+
+    private RecoveryRoomRepository recoveryRoomRepository;
+
+	@Autowired
+	public RecoveryRoomService(RecoveryRoomRepository recoveryRoomRepository) {
+		this.recoveryRoomRepository = recoveryRoomRepository;
+	}
+
     public List<RecoveryRoom> getAll(){
-        return null;
+        return recoveryRoomRepository.findAll();
     }
 
     public List<RecoveryRoomType> getAllRecoveryRoomTypes(){
-        return null;
+        return recoveryRoomRepository.findAllRecoveryRoomTypes();
     }
 
     public RecoveryRoomType getRecoveryRoomType(String typeName) {
-        return null;
+        return recoveryRoomRepository.findRecoveryRoomTypeByName(typeName);
     }
 
     public RecoveryRoom save(RecoveryRoom p) throws DuplicatedRoomNameException {
